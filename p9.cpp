7ad8eb11fca05d53d379ae7ec558d49a9a511234
hatone <b1006006@gmail.com>
2011-08-01 23:24:58 +0900

    p9

---
+++ p9.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+const int MAX_N = 50;
+
+int main(){
+    int n,m, k[MAX_N];
+    
+    //input
+    std::cin>>n>>m;
+    for (int i=0; i<n; i++) {
+        std::cin>>k[i];
+    }
+    
+    bool f = false;
+    
+    for(int a=0; a<n; a++){
+        for(int b=0; b<n; b++){
+            for(int c=0; c<n; c++){
+                for(int d=0; d<n; d++){
+                    if (k[a]+k[b]+k[c]+k[d]==m) {
+                        f=true;
+                    }
+                }
+            }
+        }
+    }
+    
+      
+
+if(f){
+    std::cout<<"yes"<<std::endl;
+    }
+    else{
+        std::cout<<"no"<<std::endl;
+    }
+    
+    
+    return 0;
+}
